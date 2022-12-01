2009462caad5554a9c061d2c202ebc8e8261e1ab
eduardlf <109638723+eduardlf@users.noreply.github.com>
2022-12-01 09:47:31 -0300

    fisrt commit
    fisrt commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'dart:io';
 
+import 'package:dartexcelconvert/widgets/home.dart';
 import 'package:flutter/material.dart';
-import 'package:excel/excel.dart';
+// import 'package:window_size/window_size.dart';
+// ignore: import_of_legacy_library_into_null_safe
+// import 'package:excel/excel.dart';
 
 void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+
+  // setWindowTitle('Flutter Demo');
+  // setWindowMinSize(const Size(400, 300));
+  // setWindowMaxSize(Size.infinite);
+
   runApp(const MyApp());
 }
 
@@ -16,33 +25,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'excel convert'),
+      home: const HomePage(title: 'excel convert'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  const MyHomePage({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
 
   final String title;
 
@@ -54,17 +48,17 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   Future<void> createExcel() async {
-    var excel = Excel.createExcel();
-    Sheet sheetObject = excel['Sheet1'];
+    // var excel = Excel.createExcel();
+    // Sheet sheetObject = excel['Sheet1'];
 
-    var cell = sheetObject.cell(CellIndex.indexByString("A1"));
-    cell.value = "pepinos " + _counter.toString();
+    // var cell = sheetObject.cell(CellIndex.indexByString("A1"));
+    // cell.value = "pepinos " + _counter.toString();
 
-    excel.encode().then((onValue) {
-      File("pepino.xlsx")
-        ..createSync(recursive: true)
-        ..writeAsBytesSync(onValue);
-    });
+    // excel.encode().then((onValue) {
+    //   File("pepino.xlsx")
+    //     ..createSync(recursive: true)
+    //     ..writeAsBytesSync(onValue);
+    // });
   }
 
   void _incrementCounter() {
---
+++ lib/widgets/home.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'O botão "+" cria um excel',
+            ),
+            Text(
+              '',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {print('asd')},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
